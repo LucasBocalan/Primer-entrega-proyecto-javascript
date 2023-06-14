dc15db96dcfeafc91de1873d57c9108bf1315d75
LucasBocalandro <Lbocalandro94@gmail.com>
2023-06-14 09:49:39 -0300

    primer entrega proyecto js parte 2

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+function alerta(){
+    var NombreUsuario = prompt ('Introduce tu nombre') ;
+    alert ('Bienvenido ' + NombreUsuario);
+
+} 
+
+
+
+
+
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
